cf579f92c7bc2c8e7d81c10bc2efefeea5ee1912
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-01-02 15:55:45 +0530

    Add files via upload

---
+++ Module5/2-1-API/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+    define('HOST','localhost'); 
+    define('USER','id20092003_ankit');
+    define('PASS','Tops@12345678');
+    define('DB','id20092003_demo');
+
+    $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+
+?>
---
+++ Module5/2-1-API/dataview.php
@@ -0,0 +1,26 @@
+<?php
+    
+    include('connection.php');
+    
+    $sql ="select * from Cricketers";
+    
+    $r = mysqli_query($con,$sql);
+    $response = array();
+    
+    while($row = mysqli_fetch_array($r))
+    {
+        $value["id"]= $row["id"];
+        $value["name"]= $row["name"];
+        $value["surname"]= $row["surname"];
+        $value["image"]= $row["image"];
+        
+    
+        array_push($response,$value);    
+    }
+    
+    echo json_encode($response);
+    mysqli_close($con);
+    
+    
+
+?>
